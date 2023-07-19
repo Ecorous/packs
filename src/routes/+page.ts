edfaecb320a86cada762f66b22143ac71a176064
Ecorous <ecorous@pridecraft.gay>
2023-07-19 17:49:21 +0100

    change adapter settings

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,20 +4,20 @@ import * as toml from 'toml';
 
 export async function load({ fetch, params }) {
     try {
-    let packs_dirs = await (await fetch("/data.json")).json()
-    var packs: any[] = []
-    var paths: any[] = []
-    packs_dirs.files.forEach(async (element: any) => {
-        let req = await fetch(`/data/${element}/pack.toml`)
-        if (req.status != 200) {
-            throw error(500, "src/routes/+page.ts:req.status was: " + req.status)
-        }
-        let packToml = await req.text()
-        console.log(packToml)
-        packs.push(toml.parse(packToml))
-        paths.push(element)
-    });
-    return {packs, paths}
+        let packs_dirs = await (await fetch("/data.json")).json()
+        var packs: any[] = []
+        var paths: any[] = []
+        packs_dirs.files.forEach(async (element: any) => {
+            let req = await fetch(`/data/${element}/pack.toml`)
+            if (req.status != 200) {
+                throw error(500, "src/routes/+page.ts:req.status was: " + req.status)
+            }
+            let packToml = await req.text()
+            console.log(packToml)
+            packs.push(toml.parse(packToml))
+            paths.push(element)
+        });
+        return { packs, paths }
     }
     catch {
         throw error(500, "you found me :P")
