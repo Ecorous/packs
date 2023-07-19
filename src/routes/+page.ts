331f02f46513632477ff4e639493e7b1ddbf9bc9
Ecorous <ecorous@pridecraft.gay>
2023-07-19 18:45:35 +0100

    try fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -7,29 +7,24 @@ export async function load({ fetch, params }) {
         let packs_dirs = await (await fetch("/data.json")).json()
         var packs: any[] = []
         var paths: any[] = []
-        packs_dirs.files.forEach((element: any) => {
-            let req = fetch(`/data/${element}/pack.toml`).then(
-                (nreq) => {
-                    if (nreq.status != 200) {
-                        throw error(500, "src/routes/+page.ts:req.status was: " + nreq.status)
-                    }
-                    let packToml = nreq.text().then(
-                        (npackToml) => {
-                            let nextToml = toml.parse(npackToml)
-                            packs.push(nextToml)
-                            console.log("nextToml: " + JSON.stringify(nextToml))
-                            paths.push(element)
-                            console.log("element: ", element)
-                        }
-                    )
-
-                }
-            )
-
+        packs_dirs.files.forEach(async (element: any) => {
+            let req = await fetch(`/data/${element}/pack.toml`)
+            if (req.status != 200) {
+                throw error(500, "src/routes/+page.ts:req.status was: " + req.status)
+            }
+            let packToml = await req.text()
+            let nextToml = toml.parse(packToml)
+            packs.push(nextToml)
+            console.log("nextToml: " + JSON.stringify(nextToml))
+            paths.push(element)
+            console.log("element: ", element)
         });
-        console.log("+page.ts:packs: " + packs)
-        console.log("+page.ts:paths: " + paths)
-        return { packs, paths }
+        setTimeout(() => {
+            console.log("+page.ts:packs: " + packs)
+            console.log("+page.ts:paths: " + paths)
+            return { packs, paths }
+        }, 2000)
+
     }
     catch {
         throw error(500, "you found me :P")
