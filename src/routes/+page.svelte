4e882eaeb11a43baabd86708101ad5d18c7c9951
Ecorous <ecorous@pridecraft.gay>
2023-07-19 18:21:45 +0100

    remove all functional from .svelte to try and locate issue

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    /** @type {import('./$types').PageData} */
-    export let data: any;
+    ///** @type {import('./$types').PageData} */
+    //export let data: any;
 </script>
 <p>this is site is by ecorous</p>
 <p>it was very crappily made</p>
 <p>have fun!</p>
 <br/>
 list of packwiz packs (click the name to go to it's page)
-<p style="display:none">{data.packs[0].name}</p>
-<!--{#each data.packs as pack, i}
+<!--<p style="display:none">{data.packs[0].name}</p>
+{#each data.packs as pack, i}
     <h3><a href={"/pack/" + data.paths[i]}>{pack.name} (id: {data.paths[i]})</a></h3>
 {/each}-->
 
