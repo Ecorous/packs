d0a90e01e5c0e4764b6433c57c9e60e30e21185d
Ecorous <ecorous@pridecraft.gay>
2023-07-19 17:44:54 +0100

    add error handle

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,6 +3,7 @@ import * as toml from 'toml';
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ fetch, params }) {
+    try {
     let packs_dirs = await (await fetch("/data.json")).json()
     var packs: any[] = []
     var paths: any[] = []
@@ -17,4 +18,8 @@ export async function load({ fetch, params }) {
         paths.push(element)
     });
     return {packs, paths}
+    }
+    catch {
+        throw error(500, "you found me :P")
+    }
 }
