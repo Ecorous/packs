6d17e128de26e09a1c1cfbc8bd7eb58dcb99c0bf
Ecorous <ecorous@pridecraft.gay>
2023-07-19 18:53:56 +0100

    waa

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,17 +3,23 @@ import * as toml from 'toml';
 /** @type {import('./$types').PageLoad} */
 
 export async function load({ fetch, params }) {
+        console.log("zz")
         let packs_dirs = await (await fetch("/data.json")).json()
         var packs: any[] = []
         var paths: any[] = []
         for (const element of packs_dirs.files) {
             let req = await fetch(`/data/${element}/pack.toml`)
+            console.log("a")
             if (req.status != 200) {
                 throw error(500, "src/routes/+page.ts:req.status was: " + req.status)
             }
+            console.log("b")
             let packToml = await req.text()
+            console.log("c")
             let nextToml = toml.parse(packToml)
+            console.log("d")
             packs.push(nextToml)
+            console.log("e")
             console.log("nextToml: " + JSON.stringify(nextToml))
             paths.push(element)
             console.log("element: ", element)
