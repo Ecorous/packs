c2c966c803e06e906687808d0e180cc293cc530e
Ecorous <ecorous@pridecraft.gay>
2023-07-19 18:26:22 +0100

    switch out line to test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,10 @@
 <p>have fun!</p>
 <br/>
 list of packwiz packs (click the name to go to it's page)
-<p style="display:none">{data.packs[0].name}</p>
-<!--{#each data.packs as pack, i}
+<!--<p style="display:none">{data.packs[0].name}</p>-->
+{#each data.packs as pack, i}
     <h3><a href={"/pack/" + data.paths[i]}>{pack.name} (id: {data.paths[i]})</a></h3>
-{/each}-->
+{/each}
 
 <style>
     a {
