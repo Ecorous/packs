d4760fbaa94e892538f12e55e6cb49ea37bff09c
Ecorous <ecorous@pridecraft.gay>
2023-07-19 18:16:57 +0100

    add a ridiculous amount of console.log to test

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -4,18 +4,27 @@ import * as toml from 'toml';
 
 export async function load({ fetch, params }) {
     try {
+        console.log("cheesy cursing")
         let packs_dirs = await (await fetch("/data.json")).json()
+        console.log("oh my life")
         var packs: any[] = []
         var paths: any[] = []
+        console.log("did.you.know")
         packs_dirs.files.forEach(async (element: any) => {
+            console.log("im before the curs3d")
             let req = await fetch(`/data/${element}/pack.toml`)
+            console.log("im somewhere i curs3d")
             if (req.status != 200) {
                 throw error(500, "src/routes/+page.ts:req.status was: " + req.status)
             }
+            console.log("you see, this is issue")
             let packToml = await req.text()
+            console.log("mhm")
             console.log(packToml)
             packs.push(toml.parse(packToml))
+            console.log("ohno")
             paths.push(element)
+            console.log(":catstair:")
         });
         return { packs, paths }
     }
