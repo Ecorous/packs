b65c8d19d9ea472a9e69945a311d6d76c1905866
Ecorous <ecorous@pridecraft.gay>
2023-07-20 19:22:25 +0100

    add echo to build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+echo "Running MMC Pack generation"
 python3 build-scripts/mmc.py &&
+echo "Running json file generation"
 python3 build-scripts/generate-list.py &&
+echo "Building site"
 npm run build
+echo "✨ Done!"
